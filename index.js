201e935b5d92075d3d9acda56f8d2df4ad77e953
lornahWambuiGitau <lornah.gitau@student.moringaschool.com>
2023-09-18 18:53:37 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+console.log(multiply); 
+
+const random = Math.floor(Math.random() * (Number.MAX_SAFE_INTEGER - 1)) + 1;
+
+console.log(random);
+
+const num3 = 20; 
+const num4 = 8;  
+
+const mod = (num3 % num4);
+
+console.log(mod);
+
+const numbers = [12, 1, 20, 17, 10]; 
+
+const max = Math.max(...numbers);
+
+console.log(max);
